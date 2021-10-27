ebe739f2d66b4991858076b6078d06ee3ca0113f
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-27 21:43:08 +0300

    add my auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,66 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { TOKEN_SECRET, COOKIE_NAME } = require('../config')
+
+const userService = require('../services/userService');
+
+
+function init() {
+    return (req, res, next) => {
+        //atach functions to context;
+        req.auth = {
+            async register(username, password) {
+                const token = await register(username, password);
+                res.cookie(COOKIE_NAME, token);
+            },
+            login() {
+                const token = await login(username, password);
+                res.cookie(COOKIE_NAME, token);
+            },
+            logout() {
+                res.clearCookie(COOKIE_NAME);
+            }
+        }
+        next();
+    };
+};
+
+
+async function register(username, password) {
+    //TODO adapt parameters to project requirments
+    //TODO extra validations
+    const existing = await userService.getUserByUsername(username);
+    if (existing) {
+        throw new Error('Username is taken!');
+    };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userService.createUser(username, hashedPassword);
+    //TODO log in user
+    return generateToken(user);
+};
+
+async function login(username, password) {
+    const user = await userService.getUserByUsername(username);
+    if (!user) {
+        throw new Error('No such user');
+    };
+
+    const hasMatch = await bcrypt.compare(password, user.hashedPassword);
+    if (!hasMatch) {
+        throw new Error('Incorrect password')
+    };
+
+    return generateToken(user);
+
+};
+
+
+
+function generateToken(userData) {
+    return jwt.sign({
+        _id: userData._id,
+        username: userData.username
+    }, TOKEN_SECRET);
+};
