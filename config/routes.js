cabbe8ede9594fa961564937e9cd2c9075339ad2
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-28 21:44:21 +0300

    added my home page view and homeController

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const authController = require('../controllers/authController');
+const homeController = require('../controllers/homeController');
 module.exports = (app) => {
+    app.use('/', homeController);
     app.use('/auth', authController);
 };
