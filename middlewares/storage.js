9a7a533534b96784563d501cb2fc7e206e205aa7
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-28 22:52:03 +0300

    import in our express application as a middleware the storageMiddleware and use it

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -1,6 +1,6 @@
 const hotel = require('../services/hotelService');
 
-module.exports = (req, res, next) => {
+module.exports = () => (req, res, next) => {
     //TODO import and decorate services
     req.storage = {
         ...hotel
