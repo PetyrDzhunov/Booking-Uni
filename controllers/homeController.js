bc103bfd62f46ab1b24cfe9cc3c32e98f37d9a06
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-28 23:20:57 +0300

    added create hotels functionality and render it at homePage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 
 router.get('/', async(req, res) => {
-    const hotels = await req.storage.getAllHotels;
+    const hotels = await req.storage.getAllHotels();
+    console.log(hotels);
     res.render('home/home', { hotels });
 });
 
--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,8 +1,46 @@
+const { isUser } = require('../middlewares/guards');
+
 const router = require('express').Router();
 
-router.get('/create', async(req, res) => {
+router.get('/create', isUser(), async(req, res) => {
     res.render('hotel/create');
 });
+
+router.post('/create', isUser(), async(req, res) => {
+    const hotelData = {
+        name: req.body.name,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        rooms: req.body.rooms,
+        bookedBy: [],
+        owner: req.user._id
+    };
+
+    console.log(hotelData);
+
+    try {
+        await req.storage.createHotel(hotelData);
+        res.redirect('/')
+    } catch (error) {
+        let errors;
+        if (error.errors) {
+            errors = Object.values(error.errors).map(e => e.properties.message);
+        } else {
+            errors = [error.message];
+        }
+        const ctx = {
+            errors: errors,
+            hotelData: {
+                name: req.body.name,
+                city: req.body.city,
+                imageUrl: req.body.imageUrl,
+                rooms: req.body.rooms,
+            },
+        };
+        res.render('hotel/create', ctx)
+    }
+});
+
 
 
 module.exports = router;
